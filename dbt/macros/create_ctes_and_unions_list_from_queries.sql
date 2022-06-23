b82311b46d2cd7f37acb1fcf0d3f11debed76c57
admin@erad.co <admin@erad.co>
2022-06-23 14:28:21 +0000

    saving update

--- dbt/macros/create_ctes_and_unions_list_from_queries.sql
+++ dbt/macros/create_ctes_and_unions_list_from_queries.sql
@@ -1,4 +1,4 @@
-{% macro create_ctes_and_unions_list_from_tables(cte_names, queries) %}
+{% macro create_ctes_and_unions_list_from_queries(cte_names, queries) %}
     {% set ctes = [] %}
     {% set cte_selects = [] %}
     {% for table_name in tables %}
@@ -6,7 +6,7 @@
         {% set cte_name = cte_names[i] %}
 
         {{ cte_selects.append('SELECT * FROM ' ~ cte_name) }}
-        {{ set cte = queries[i] }}
+        {% set cte = queries[i] %}
 
         {% set cte = create_cte_from_source_sql(required_columns, schema_name, table_name, cte_name, where_clause=where_clause) %}
         {{ ctes.append(cte) }}
