23e2d891061eb006024678cbec351dc82c5b96f4
admin@erad.co <admin@erad.co>
2022-06-26 02:40:12 +0000

    Adding Tap Payments

--- dbt/models/intermediates/int_revenue_summary.sql
+++ dbt/models/intermediates/int_revenue_summary.sql
@@ -41,6 +41,17 @@ WITH BASE AS (
         ERAD_REVENUE                    as REVENUE
     FROM {{ref('stg_revenue_magento')}}
 
+    UNION ALL
+
+    SELECT 
+        ERAD_TIMESTAMP::DATE            as DATE,
+        ERAD_CUSTOMER_ID                as CUSTOMER_ID,
+        'TAP Payments'                  as REVENUE_SOURCE,
+        'USD'                           as CURRENCY_CODE,
+        CONCAT(ERAD_SCHEMA,'.',ERAD_TABLE) as ERAD_SOURCE,
+        ERAD_REVENUE                    as REVENUE
+    FROM {{ref('stg_revenue_tappayments')}}
+
 )
     SELECT 
         DATE,
