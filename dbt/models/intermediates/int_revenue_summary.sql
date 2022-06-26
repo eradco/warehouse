4497baa72e3c98dd1fd3a2eacdb91f1505c85151
admin@erad.co <admin@erad.co>
2022-06-26 00:33:54 +0000

    adding magento to model

--- dbt/models/intermediates/int_revenue_summary.sql
+++ dbt/models/intermediates/int_revenue_summary.sql
@@ -30,6 +30,17 @@ WITH BASE AS (
         ERAD_REVENUE                    as REVENUE
     FROM {{ref('stg_revenue_shopify')}}
 
+    UNION ALL
+
+    SELECT 
+        ERAD_TIMESTAMP::DATE            as DATE,
+        ERAD_CUSTOMER_ID                as CUSTOMER_ID,
+        'Magento'                       as REVENUE_SOURCE,
+        'USD'                           as CURRENCY_CODE,
+        CONCAT(ERAD_SCHEMA,'.',ERAD_TABLE) as ERAD_SOURCE,
+        ERAD_REVENUE                    as REVENUE
+    FROM {{ref('stg_revenue_magento')}}
+
 )
     SELECT 
         DATE,
