a0773288ba81928a2eb696de929509608a49cd12
admin@erad.co <admin@erad.co>
2022-06-30 02:21:34 +0000

    adding facebook

--- dbt/models/intermediates/int_ads_summary.sql
+++ dbt/models/intermediates/int_ads_summary.sql
@@ -26,6 +26,20 @@ BASE AS (
         0                       as ACQUSITIONS,
         ERAD_COST               as COST
     FROM {{ ref('stg_ads_tiktok') }}
+
+    UNION ALL
+
+    SELECT 
+        ERAD_TIMESTAMP::DATE    as Date,
+        ERAD_CUSTOMER_ID        as CUSTOMER_ID,
+        'Facebook Ads'          as AD_CHANNEL,
+        'USD'                   as CURRENCY_CODE,
+        CONCAT(ERAD_SCHEMA,'.',ERAD_TABLE) as ERAD_SOURCE,
+        0                       as CONVERSIONS,
+        CLICKS                  as CLICKS,
+        0                       as ACQUSITIONS,
+        ERAD_COST               as COST
+    FROM {{ ref('stg_ads_facebook') }}
     )
 
 
